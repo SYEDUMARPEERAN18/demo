7d25fa3c381e0291c553704b73da9106bf7bf879
SYEDUMARPEERAN18 <umaraids386@gmail.com>
2025-12-15 10:59:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a=14
+b=30
+c=47
+
+print("-----------------------")
+print("additions:",a+b+c)
+print("subractions:",a-b-c)
+print("devitions:",a/b/c)
+print("multiplication:",a*b*c)
+print("additions:",a+b+c)
